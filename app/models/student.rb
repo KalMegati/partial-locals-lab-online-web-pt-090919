bacde47c4f1c242733549d50d5ae4aa2309ddf49
kalmegati <kalmegati@gmail.com>
2020-02-15 13:41:25 -0500

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,15 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(query)
+    if query.length > 0
+      self.all.map { |student|
+        student if student.name.match(/#{query}/i)
+      }.compact
+    else
+      self.all
+    end
+  end
+
 end
